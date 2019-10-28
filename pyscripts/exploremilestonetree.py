1a349a63179435d7118e584df36c064d7b12121a
Peti <pmernyei@gmail.com>
2019-10-28 17:50:24 +0000

    Refactored python scripts to make them more reuseable

--- pyscripts/exploremilestonetree.py
+++ pyscripts/exploremilestonetree.py
@@ -2,40 +2,54 @@ import csv
 import sys
 import json
 
-raw_sizes = {}
-parent = {}
-children = {}
-roots = []
+def calculate_milestone_tree(sizes_filename, subcats_filename, out_filename=None):
+    raw_sizes = {}
+    parent = {}
+    children = {}
+    roots = []
 
-with open(sys.argv[1]) as size_file:0
-    reader = csv.reader(size_file, delimiter='\t')
-    next(reader, None) # Skip header
-    for category, size in reader:
-        raw_sizes[category] = int(size)
+    with open(sizes_filename) as size_file:
+        reader = csv.reader(size_file, delimiter='\t')
+        next(reader, None) # Skip header
+        for category, size in reader:
+            raw_sizes[category] = int(size)
 
-with open(sys.argv[2]) as tree_file:
-    reader = csv.reader(tree_file, delimiter='\t')
-    for category, p in reader:
-        if p != 'null':
-            parent[category] = p
-            if p in children:
-                children[p].append(category)
+    with open(subcats_filename) as tree_file:
+        reader = csv.reader(tree_file, delimiter='\t')
+        for category, p in reader:
+            if p != 'null':
+                parent[category] = p
+                if p in children:
+                    children[p].append(category)
+                else:
+                    children[p] = [category]
             else:
-                children[p] = [category]
-        else:
-            roots.append(category)
+                roots.append(category)
 
-aggregated_sizes = {}
+    aggregated_sizes = {}
 
-def aggregate(cat):
-    size = raw_sizes.get(cat, 0)
-    for child in children.get(cat, []):
-        size += aggregate(child)
-    aggregated_sizes[cat] = size
-    return size
+    def aggregate(cat):
+        size = raw_sizes.get(cat, 0)
+        for child in children.get(cat, []):
+            size += aggregate(child)
+        aggregated_sizes[cat] = size
+        return size
 
-for root in roots:
-    aggregate(root)
+    for root in roots:
+        aggregate(root)
 
-with open('tmp/data.json', 'w+', encoding='utf8') as outfile:
-    outfile.write(json.dumps({'raw_sizes': raw_sizes, 'parent': parent, 'children': children, 'roots': roots, 'aggregated_sizes': aggregated_sizes}))
+    result = {
+        'raw_sizes': raw_sizes,
+        'parent': parent,
+        'children': children,
+        'roots': roots,
+        'aggregated_sizes': aggregated_sizes
+    }
+
+    if out_filename:
+        open(out_filename, 'w+', encoding='utf8').write(json.dumps(result))
+    return result
+
+
+if __name__ == '__main__':
+    calculate_milestone_tree(sys.argv[1], sys.argv[2], sys.argv[3])
--- pyscripts/extractcategorysizes.py
+++ pyscripts/extractcategorysizes.py
@@ -1,28 +1,37 @@
 import re
 import pandas as pd
+import sys
 
-def file_len(fname):
-    with open(fname) as f:
-        for i, l in enumerate(f):
-            pass
-    return i + 1
+def get_category_sizes(page2cat_filename, output_filename=None):
+
+    def file_len(fname):
+        with open(fname) as f:
+            for i, l in enumerate(f):
+                pass
+        return i + 1
 
 
-FILE_IN = '..\\output\\page2cat.tsv'
-FILE_OUT = '..\\output\\category_sizes.tsv'
+    FILE_IN = '..\\output\\page2cat.tsv'
+    FILE_OUT = '..\\output\\category_sizes.tsv'
 
-counts = {}
-idx = 0
-for line in open(FILE_IN, encoding='utf8'):
-    titles = re.split(r'\t+', line)
-    for i in range(1, len(titles)-1):
-        title = titles[i]
-        counts[title] = counts.get(title, 0) + 1
-    idx += 1
-    if idx % 100000 == 0:
-        print(idx, 'pages counted')
+    counts = {}
+    idx = 0
+    for line in open(page2cat_filename, encoding='utf8'):
+        titles = re.split(r'\t+', line)
+        for i in range(1, len(titles)-1):
+            title = titles[i]
+            counts[title] = counts.get(title, 0) + 1
+        idx += 1
+        if idx % 100000 == 0:
+            print(idx, 'pages counted')
 
-df = pd.DataFrame([{'category': k, 'pages': v} for (k,v) in counts.items()])
-df = df.sort_values(by='pages', ascending=False)
-df.to_csv(FILE_OUT, sep='\t', encoding='utf-8', index=False)
-print('Counted', len(counts), 'category sizes')
+    print('Counted', len(counts), 'category sizes')
+    if output_filename:
+        df = pd.DataFrame([{'category': k, 'pages': v} for (k,v) in counts.items()])
+        df = df.sort_values(by='pages', ascending=False)
+        df.to_csv(FILE_OUT, sep='\t', encoding='utf-8', index=False)
+
+    return counts
+
+if __name__ == '__main__':
+    get_category_sizes(sys.argv[1], sys.argv[2])
