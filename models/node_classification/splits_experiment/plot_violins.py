bb6466f09b7b278aaf71a0bc4c31c96e80c3a20d
Peter Mernyei <pmernyei@gmail.com>
2020-04-13 22:06:56 +0200

    Plot split expriment results

--- models/node_classification/splits_experiment/plot_violins.py
+++ models/node_classification/splits_experiment/plot_violins.py
@@ -5,12 +5,19 @@ import argparse
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='splits-experiment-plot')
     parser.add_argument('--results-file', help='file to read results from')
-    parser.add_argument('--out', help='file to read results from', default='violinplot.png')
+    parser.add_argument('--out', help='file to read results from',
+        default='violinplot.png')
     args = parser.parse_args()
 
     results = pd.read_pickle(args.results_file)
+    results['accuracy_delta'] = results.accuracy
+    for model in results.model.unique():
+        mask = (results.model == model)
+        avg = results[mask].accuracy.mean()
+        results.loc[mask, 'accuracy_delta'] -= avg
+
     sns.set(style="whitegrid")
-    plot = sns.violinplot(x='split', y='accuracy', hue='model', data=results,
-        palette='muted')
+    plot = sns.violinplot(x='split', y='accuracy_delta', hue='model',
+        data=results, palette='muted')
     fig = plot.get_figure()
     fig.savefig(args.out)
